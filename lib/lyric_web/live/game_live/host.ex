419c008c12f799a7f495676680886bf8727e9f44
Thierry Schmidt <thierry@whatwedo.ch>
2025-02-12 16:38:52 +0100

    add qr code to join game

--- lib/lyric_web/live/game_live/host.ex
+++ lib/lyric_web/live/game_live/host.ex
@@ -6,17 +6,28 @@ defmodule LyricWeb.GameLive.Host do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket |> assign(:page_title, "Host Game")}
+    {:ok, socket |> assign(:page_title, "Host Game") |> assign(:qr_code, "")}
   end
 
   @impl true
   def handle_params(%{"id" => id}, _uri, socket) do
     game = Playground.get_game_with_song!(id)
+    {:ok, qr_code} = get_qr_code(id)
 
     IO.inspect(game)
 
     {:noreply,
      socket
-     |> assign(:game, game)}
+     |> assign(:game, game)
+     |> assign(:qr_code, qr_code)}
+  end
+
+  defp get_qr_code(id) do
+    joining_url = LyricWeb.Endpoint.url() <> "/games/#{id}/play"
+
+    joining_url
+    |> QRCode.create(:medium)
+    |> QRCode.render()
+    |> QRCode.to_base64()
   end
 end
--- mix.exs
+++ mix.exs
@@ -57,7 +57,8 @@ defmodule Lyric.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:qr_code, "~> 3.1.0"}
     ]
   end
 
