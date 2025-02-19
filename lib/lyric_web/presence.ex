f8aab959da41d085ec56e6c1b280edefd484380d
Thierry Schmidt <thierry@whatwedo.ch>
2025-02-19 16:17:46 +0100

    fix presence

--- lib/lyric_web/presence.ex
+++ lib/lyric_web/presence.ex
@@ -11,8 +11,22 @@ defmodule LyricWeb.Presence do
   alias LyricWeb.Endpoint
 
   @player_activity_prefix_topic "player_activity:"
+  @colors [
+    "#E6F2FF",
+    "#FFEBF0",
+    "#F0FFF0",
+    "#FFF0E6",
+    "#F2E6FF",
+    "#FFFFD6",
+    "#E6FFFA",
+    "#FFE6F0",
+    "#F5FFE6",
+    "#E6F0FF",
+    "#FFF5E6",
+    "#F0E6FF"
+  ]
 
-  def subscribeToPlayerActivity(game_id) do
+  def subscribe_to_player_activity(game_id) do
     topic = @player_activity_prefix_topic <> game_id
 
     Endpoint.subscribe(topic)
@@ -20,15 +34,19 @@ defmodule LyricWeb.Presence do
 
   def track_player(pid, game_id, player_name) do
     topic = @player_activity_prefix_topic <> game_id
+    color = Enum.random(@colors)
 
     Presence.track(pid, topic, "players", %{
-      name: player_name
+      name: player_name,
+      color: color
     })
   end
 
   def list_players(game_id) do
     topic = @player_activity_prefix_topic <> game_id
 
-    Presence.list(topic) |> get_in(["players", :metas]) || []
+    players = Presence.list(topic) |> get_in(["players", :metas]) || []
+    IO.inspect(players)
+    players
   end
 end
