c60a7b9cdeb5dd0d69b11a0e83892d3664b023d2
Thierry Schmidt <thierry@whatwedo.ch>
2025-02-19 16:18:20 +0100

    track players

--- lib/lyric_web/live/game_live/host.ex
+++ lib/lyric_web/live/game_live/host.ex
@@ -1,25 +1,41 @@
 defmodule LyricWeb.GameLive.Host do
   use LyricWeb, :live_view
 
+  alias LyricWeb.Presence
   alias Lyric.Playground
-  alias Lyric.Musics
 
   @impl true
-  def mount(_params, _session, socket) do
-    {:ok, socket |> assign(:page_title, "Host Game") |> assign(:qr_code, "")}
-  end
-
-  @impl true
-  def handle_params(%{"id" => id}, _uri, socket) do
+  def mount(%{"id" => id}, _session, socket) do
     game = Playground.get_game_with_song!(id)
     {:ok, qr_code} = get_qr_code(id)
 
-    IO.inspect(game)
+    if not connected?(socket) do
+      {:ok,
+       socket
+       |> assign(:page_title, "Host Game")
+       |> assign(:game, game)
+       |> assign(:game_state, :waiting)
+       |> assign(:players, [])
+       |> assign(:qr_code, qr_code)}
+    end
 
-    {:noreply,
+    Presence.subscribe_to_player_activity(id)
+    players = Presence.list_players(id)
+
+    {:ok,
      socket
+     |> assign(:page_title, "Host Game")
      |> assign(:game, game)
+     |> assign(:game_state, :waiting)
+     |> assign(:players, players)
      |> assign(:qr_code, qr_code)}
+  end
+
+  @impl true
+  def handle_info(%{event: "presence_diff"}, socket) do
+    players = Presence.list_players(to_string(socket.assigns.game.id))
+
+    {:noreply, socket |> assign(:players, players)}
   end
 
   defp get_qr_code(id) do
--- lib/lyric_web/live/game_live/play.ex
+++ lib/lyric_web/live/game_live/play.ex
@@ -1,11 +1,38 @@
 defmodule LyricWeb.GameLive.Play do
   use LyricWeb, :live_view
 
-  alias Lyric.Playground
-  alias Lyric.Musics
+  alias LyricWeb.Presence
+  alias LyricWeb.Endpoint
+
+  @game_prefix_topic "game_session:"
 
   @impl true
-  def mount(_params, _session, socket) do
-    {:ok, socket |> assign(:page_title, "Play Game")}
+  def mount(params, _session, socket) do
+    game_id = Map.get(params, "id")
+
+    {:ok,
+     socket
+     |> assign(:page_title, "Play Game")
+     |> assign(:game_id, game_id)
+     |> assign(:status, :joining)
+     |> assign(:form, to_form(%{"player_name" => ""}))
+     |> assign(:player_name, "")}
+  end
+
+  @impl true
+  @spec handle_event(<<_::72>>, map(), Phoenix.LiveView.Socket.t()) :: {:noreply, map()}
+  def handle_event("join_game", %{"player_name" => player_name}, socket) do
+    maybe_join_game_as_player(socket, player_name)
+
+    {:noreply, socket |> assign(:player_name, player_name) |> assign(:status, :waiting)}
+  end
+
+  defp maybe_join_game_as_player(socket, player_name) do
+    if connected?(socket) do
+      game_topic = @game_prefix_topic <> socket.assigns.game_id
+
+      Presence.track_player(self(), socket.assigns.game_id, player_name)
+      Endpoint.subscribe(game_topic)
+    end
   end
 end
