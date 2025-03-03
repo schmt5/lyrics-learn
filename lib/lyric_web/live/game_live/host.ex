7f80a0b1045f630c76cfaa77a8487317eb625aca
Thierry Schmidt <thierry@whatwedo.ch>
2025-03-03 17:45:46 +0100

    lyrics

--- lib/lyric_web/live/game_live/host.ex
+++ lib/lyric_web/live/game_live/host.ex
@@ -14,14 +14,14 @@ defmodule LyricWeb.GameLive.Host do
     {:ok, qr_code} = get_qr_code(id)
 
     lyrics = %{
-      initial_timeout: 3100,
+      initial_timeout: 3000,
       lines: [
         %{
           text: "I found a love for me",
           word_to_guess: "love",
           options: ["life", "love", "heart", "song"],
           correct_index: 1,
-          timeout: 7800
+          timeout: 7700
         },
         %{
           text: "Oh, darling, just dive right in and follow my lead",
@@ -84,28 +84,98 @@ defmodule LyricWeb.GameLive.Host do
           word_to_guess: "dancing",
           options: ["swaying", "dancing", "singing", "standing"],
           correct_index: 1,
-          timeout: 9800
+          timeout: 10000
         },
         %{
           text: "Barefoot on the grass, listening to our favorite song",
           word_to_guess: "favorite",
           options: ["special", "favorite", "chosen", "perfect"],
           correct_index: 1,
-          timeout: 7300
+          timeout: 7950
         },
         %{
           text: "When you said you looked a mess, I whispered underneath my breath",
           word_to_guess: "whispered",
           options: ["whispered", "muttered", "mumbled", "said"],
           correct_index: 0,
-          timeout: 7400
+          timeout: 7600
         },
         %{
           text: "But you heard it, darling, you look perfect tonight",
           word_to_guess: "perfect",
           options: ["beautiful", "gorgeous", "amazing", "perfect"],
           correct_index: 3,
-          timeout: 10000
+          timeout: 13500
+        },
+        %{
+          text: "Well I found a woman, stronger than anyone I know",
+          word_to_guess: "stronger",
+          options: ["better", "kinder", "stronger", "wiser"],
+          correct_index: 2,
+          timeout: 7100
+        },
+        %{
+          text: "She shares my dreams, I hope that someday I'll share her home",
+          word_to_guess: "dreams",
+          options: ["life", "dreams", "future", "world"],
+          correct_index: 1,
+          timeout: 8000
+        },
+        %{
+          text: "I found a love, to carry more than just my secrets",
+          word_to_guess: "secrets",
+          options: ["burdens", "secrets", "sorrows", "worries"],
+          correct_index: 1,
+          timeout: 9100
+        },
+        %{
+          text: "To carry love, to carry children of our own",
+          word_to_guess: "children",
+          options: ["memories", "promises", "children", "future"],
+          correct_index: 2,
+          timeout: 5300
+        },
+        %{
+          text: "We are still kids, but we're so in love",
+          word_to_guess: "kids",
+          options: ["young", "kids", "dreamers", "growing"],
+          correct_index: 1,
+          timeout: 4500
+        },
+        %{
+          text: "Fighting against all odds",
+          word_to_guess: "odds",
+          options: ["odds", "challenges", "obstacles", "doubts"],
+          correct_index: 0,
+          timeout: 4500
+        },
+        %{
+          text: "I know we'll be alright this time",
+          word_to_guess: "alright",
+          options: ["happy", "together", "alright", "perfect"],
+          correct_index: 2,
+          timeout: 7000
+        },
+        %{
+          text: "Darling, just hold my hand",
+          word_to_guess: "hold",
+          options: ["take", "hold", "grasp", "touch"],
+          correct_index: 1,
+          timeout: 7000
+        },
+        %{
+          text: "Be my girl, I'll be your man",
+          word_to_guess: "girl",
+          options: ["girl", "love", "wife", "partner"],
+          correct_index: 0,
+          timeout: 7000
+        },
+        %{
+          text: "I see my future in your eyes",
+          word_to_guess: "future",
+          options: ["dreams", "hope", "love", "future"],
+          correct_index: 3,
+          timeout: 100000
         }
       ]
     }
